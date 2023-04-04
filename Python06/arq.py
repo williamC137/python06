486c863424782b0c8d4c1ee1b9566b03df319476
williamC137 <114762784+williamC137@users.noreply.github.com>
2023-04-04 09:17:48 -0300

    Add files via upload

---
+++ Python06/arq.py
@@ -0,0 +1,38 @@
+import sqlite3
+from modelos import Pessoa, Marca, Veiculo
+
+#arquivo de como vai se chamar o banco
+banco = sqlite3.connect('database.db')
+banco.execute ("PRAGMA foreign_keys=on")
+#para escrever em sql, para inserir os dados, deletar, as manipulações no banco
+cursor = banco.cursor()
+
+
+# cursor.execute("CREATE TABLE Pessoa(cpf text, nome text, nascimento text)")
+
+
+# cursor.execute("CREATE TABLE Marca(nome text, sigla text)")
+
+
+# cursor.execute("CREATE TABLE Veiculo (placa text, ano integer, cor text)")
+
+# pessoa1 = Pessoa('4324454355','William','2004')
+# pessoa1.cpf = 4324454355
+# pessoa1.nome = 'William'
+# pessoa1.nascimento = 2004
+pessoas = [
+    Pessoa('54654656', 'William', '2004'),
+    Pessoa('54767666', 'Pedro', '1999')
+]
+
+comando = '''INSERT INTO Pessoa (cpf, nome, nascimento) VALUES (?, ?, ?);'''
+cursor.executemany(comando,[(i.cpf, i.nome, i.nascimento) for i in pessoas])
+# Salvar as alterações feitas nas tabela
+banco.commit()
+# VALUES(:cpf, :nome, :nascimento)
+# cursor.execute(comando, vars(pessoas))
+# print(vars(pessoas))
+
+
+
+
---
+++ Python06/modelos.py
@@ -0,0 +1,15 @@
+class Pessoa:
+    def __init__(self, cpf, nome, nascimento):
+        self.cpf =  cpf
+        self.nome = nome
+        self.nascimento = nascimento
+class Marca: 
+    def __init__ (self, nome, sigla):
+        self.nome = nome
+        self.sigla = sigla
+class Veiculo: 
+    def __init__(self, placa, ano, cor ):
+        self.placa = placa
+        self.ano = ano
+        self.cor= cor
+    
